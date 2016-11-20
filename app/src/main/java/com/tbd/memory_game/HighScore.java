a82b9c22ad1a94f235bac40b09b45a76644b2257
Adrian Cuellar <aacuellar14@gmail.com>
2016-11-20 00:11:40 -0800

    Basic UI
    Created the basic ideas of the screens with some button functionality

---
+++ app/src/main/java/com/tbd/memory_game/HighScore.java
@@ -0,0 +1,171 @@
+package com.tbd.memory_game;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/******************************************************************************
+ * file: HighScore.java
+ * author: Miraj, Khanh, Adrian
+ * class: CS 245 - Programming Graphical User Interface
+
+ * assignment: Android App Project
+ * date last modified : 11/18/2016
+ *
+ * purpose: This class will represent each separate game's top 3 highscores.
+ ******************************************************************************/
+
+public class HighScore {
+
+    private int highScore;
+    private String[] allHS;
+    private String highScoreFileName;
+    private File file;
+
+    /* Public,basic constructor. Initalizes HighScore and all variables.
+    *
+    */
+    public HighScore(String fileName){
+        highScore=0;
+        allHS = new String[3];
+        highScoreFileName=fileName;
+        file= new File(highScoreFileName);
+        createInitialHS();
+        importScores();
+        Sort();
+    }
+    /*
+    *Creates the HighScores files if it doesn't already exist.
+    */
+    private void createInitialHS(){
+        if(file.exists()==false){
+            try {
+                file.createNewFile();
+                FileWriter writer= new FileWriter(file);
+                String placeholderScores = "abc...0";
+                for(int i=0;i<5;i++){
+                    writer.write(placeholderScores);
+                    writer.write(System.getProperty("line.separator"));
+                }
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("No highscores");
+            }
+        }
+    }
+    /*
+    *Meant to be called when the program is called. Saves the contents of allHS to the file.
+    */
+    public void createExitHS(){
+        try {
+            file.createNewFile();
+            FileWriter writer= new FileWriter(file);
+            for(int i=0;i<5;i++){
+                String scores = allHS[i];
+                writer.write(scores);
+                writer.write(System.getProperty("line.separator"));
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("No highscores");
+        }
+    }
+    /*
+    *method: changeHighScore
+    *purpose: Increment highScore by the pointsChange parameter
+    */
+
+    public void changeHighScore(int pointsChange){
+        highScore+=pointsChange;
+    }
+
+    /*
+    *method: setHighScore
+    *purpose: Manually set highScore to be the newScore parameter.
+    */
+    public void setHighScore(int newScore){
+        highScore=newScore;
+    }
+    /*
+    *method: getHighScore
+    *purpose: Returns highScore
+    */
+    public int getHighScore(){
+        return highScore;
+    }
+    /*
+    *method: addScore
+    *purpose:Adds the score to the highscore list and appropriately bumps all other
+    *scores down.
+    * according to the parameter line to the parameter newScore
+    */
+    public void addScore(String initial){
+        int line=0;
+        initial=initial+"..."+highScore;
+        for(int place=0;place<allHS.length;place++){
+            if (!allHS[place].isEmpty()) {
+                String[] separatedScore1 = allHS[place].split("\\.\\.\\.");
+                String[] separatedScore2 = initial.split("\\.\\.\\.");
+                if(Integer.parseInt(separatedScore1[1]) <= Integer.parseInt(separatedScore2[1])){
+                    line = place;
+                    break;
+                }
+            }
+        }
+        for(int place=line;place<allHS.length;place++){
+            String temp =allHS[place];
+            allHS[place]=initial;
+            initial=temp;
+        }
+    }
+    /*
+    *method: getFileScore
+    *purpose: Returns the specific highscore on the line
+    *according to the parameter
+    */
+    public String getScore(int line){
+        return allHS[line];
+    }
+    /*
+    * Sorts the array allHS into descending order
+    */
+    public void Sort(){
+        String temp="";
+        for(int place=1;place<allHS.length;place++) {
+            if (!allHS[place].isEmpty()) {
+                temp=allHS[place];
+                int place2=place-1;
+                String[] separatedScore1 = temp.split("\\.\\.\\.");
+                String[] separatedScore2 = allHS[place2].split("\\.\\.\\.");
+                while(place2>=0 && Integer.parseInt(separatedScore1[1]) > Integer.parseInt(separatedScore2[1])){
+                    allHS[place2+1]=allHS[place2];
+                    place2-=1;
+                    if(place2>=0)
+                        separatedScore2 = allHS[place2].split("\\.\\.\\.");
+                }
+                allHS[place2+1]=temp;
+            }
+        }
+    }
+    /*
+    *method: importScores
+    *purpose: Accesses all highScores on the file and moves them to allHS
+    */
+    private void importScores(){
+        String hs="";
+        int counter=0;
+        try{
+            Scanner reader = new Scanner(file);
+            while(reader.hasNextLine()){
+                hs=reader.nextLine();
+                allHS[counter]=hs;
+                counter++;
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("No such file exists. Try again");
+        }
+    }
+}
--- app/src/main/java/com/tbd/memory_game/MainActivity.java
+++ app/src/main/java/com/tbd/memory_game/MainActivity.java
@@ -2,6 +2,9 @@ package com.tbd.memory_game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startGame = (Button) findViewById(R.id.startButton);
+        startGame.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0){
+                Intent nextScreen = new Intent(getApplicationContext(),GameActivity.class);
+                startActivity(nextScreen);
+            }
+        });
+
+        Button highScoreScreen = (Button) findViewById(R.id.highScoresButton);
+        highScoreScreen.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0){
+                Intent nextScreen = new Intent(getApplicationContext(),HighScores.class);
+                startActivity(nextScreen);
+            }
+        });
+
     }
 }
