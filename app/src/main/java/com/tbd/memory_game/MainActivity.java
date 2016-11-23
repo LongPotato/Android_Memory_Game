f1ed83cba06a6ea77edffd3ebeee0b93c370dad8
Adrian Cuellar <aacuellar14@gmail.com>
2016-11-22 18:35:03 -0800

    Added @string usage

--- app/src/main/java/com/tbd/memory_game/MainActivity.java
+++ app/src/main/java/com/tbd/memory_game/MainActivity.java
@@ -7,7 +7,6 @@ import android.widget.Button;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
