a1ef8f51ee61b11a218ebd211671c948bcbfc0a2
LongPotato <bkh4nh@gmail.com>
2016-11-24 01:11:16 -0800

    fixed landscape mode for main menu screen

--- app/src/main/java/com/tbd/memory_game/MainActivity.java
+++ app/src/main/java/com/tbd/memory_game/MainActivity.java
@@ -4,9 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.content.Intent;
 
